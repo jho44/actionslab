cfe6339b45b19f3152ab63ed942b83ffad5a7178
Jessica Ho <hojessica1230@gmail.com>
2022-08-23 16:46:07 -0700

    verify validators changed

---
+++ src/nonvalidators/account-analytics-events.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ModelBase = require('./base');
+
+module.exports = ModelBase.extend({
+  tableName: 'accounts_analytics_events',
+  hasTimestamps: ['date_created', 'date_modified'],
+  serialize: function () {
+    return {
+      id: this.get('analytics_event_id'),
+      date_created: this.get('date_created'),
+      date_modified: this.get('date_modified')
+    };
+  }
+});
